93be938141898211ca6b25d4d3df1b82727dfa5f
Sam <nagbe.samuel@gmail.com>
2021-09-29 19:18:14 +0200

    [VEL-9] - renamed project name from "velo-churier" to "velochurier"

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/velo-churier'),
+      dir: require('path').join(__dirname, './coverage/velochurier'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'velo-churier'`, () => {
+  it(`should have as title 'velochurier'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('velo-churier');
+    expect(app.title).toEqual('velochurier');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('velo-churier app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('velochurier app is running!');
   });
 });
