73d7c23a49d0d059e704f98df52f9e6d5cfd715a
Sam <nagbe.samuel@gmail.com>
2021-10-04 15:50:39 +0200

    [VEL-10] - added a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DeliveriesComponent } from './deliveries/deliveries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeliveriesComponent
   ],
   imports: [
     BrowserModule
