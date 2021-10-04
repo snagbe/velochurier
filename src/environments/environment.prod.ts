5251d48e260ffea2207d9e49a652467a223e5e31
Sam <nagbe.samuel@gmail.com>
2021-10-04 15:51:44 +0200

    [VEL-11] - integrated Firebase authentication

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBCA2oaGS7PBkEV4uelWFKaQV-KdE_3iyw",
+    authDomain: "velochurier-f8254.firebaseapp.com",
+    databaseURL: "https://velochurier-f8254-default-rtdb.firebaseio.com",
+    projectId: "velochurier-f8254",
+    storageBucket: "velochurier-f8254.appspot.com",
+    messagingSenderId: "327813077860",
+    appId: "1:327813077860:web:b0562f920fe4cd99dbf871",
+    measurementId: "G-F48H5VLWC8"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBCA2oaGS7PBkEV4uelWFKaQV-KdE_3iyw",
+    authDomain: "velochurier-f8254.firebaseapp.com",
+    databaseURL: "https://velochurier-f8254-default-rtdb.firebaseio.com",
+    projectId: "velochurier-f8254",
+    storageBucket: "velochurier-f8254.appspot.com",
+    messagingSenderId: "327813077860",
+    appId: "1:327813077860:web:b0562f920fe4cd99dbf871",
+    measurementId: "G-F48H5VLWC8"
+  },
 };
 
 /*
