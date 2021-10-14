6d97f005ba42c4e6291f14872f5b9095defd682e
Sam <nagbe.samuel@gmail.com>
2021-10-14 14:34:03 +0200

    [VEL-21] - added component customers - added customer image

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Address} from "../deliveries/deliveries";
+import {DeliveriesService} from "../deliveries/deliveries.service";
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit {
+  addresses: Address[];
+  addOrder: boolean = false;
+
+  constructor(private deliveriesService: DeliveriesService) { }
+
+  ngOnInit(): void {
+    this.deliveriesService.getAdresses().subscribe(value => this.addresses = value);
+  }
+
+  openAddOrder(){
+    this.addOrder = !this.addOrder;
+  }
+}
