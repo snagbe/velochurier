b85285e805da9ec20655e108a321b4b0d755cfdb
Sam <nagbe.samuel@gmail.com>
2021-10-17 10:25:22 +0200

    [VEL-23] - added google-maps integration

---
+++ src/app/road/road.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {} from 'googlemaps';
+import { ViewChild } from '@angular/core';
+
+
+
+
+@Component({
+  selector: 'app-road',
+  templateUrl: './road.component.html',
+  styleUrls: ['./road.component.css']
+})
+
+export class RoadComponent implements OnInit {
+@ViewChild('map', {static: true}) mapElement: any;
+  map: google.maps.Map;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const mapProperties = {
+      center: new google.maps.LatLng(35.2271, -80.8431),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement,mapProperties);
+  }
+}
