0e272741563219c56e98b01ae0b651e7042e7e33
Sam <nagbe.samuel@gmail.com>
2021-10-18 21:40:05 +0200

    [VEL-24] - updated AgmCoreModule integration for switzerland

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,29 +11,31 @@ import {NavbarComponent} from './navbar/navbar.component';
 import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
 import {AngularFireStorageModule} from '@angular/fire/compat/storage';
 import {AngularFireAuthModule} from '@angular/fire/compat/auth';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 //Angular Material
-import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {MatOptionModule} from "@angular/material/core";
 import {MatInputModule} from "@angular/material/input";
-import { CustomersComponent } from './customers/customers.component';
+import {CustomersComponent} from './customers/customers.component';
 import {MatIconModule} from '@angular/material/icon';
-import { RoadComponent } from './road/road.component';
+import {RoadComponent} from './road/road.component';
 
-import { AgmCoreModule } from '@agm/core';
+import {AgmCoreModule} from '@agm/core';
+import {AutocompleteComponent} from "./autocomplete/autocomplete.component";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        DeliveriesComponent,
-        AddOrderComponent,
-        NavbarComponent,
-        CustomersComponent,
-        RoadComponent
-    ],
+  declarations: [
+    AppComponent,
+    DeliveriesComponent,
+    AddOrderComponent,
+    NavbarComponent,
+    CustomersComponent,
+    RoadComponent,
+    AutocompleteComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -56,7 +58,10 @@ import { AgmCoreModule } from '@agm/core';
 
     //Google Maps integration
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyBCA2oaGS7PBkEV4uelWFKaQV-KdE_3iyw'
+      apiKey: 'AIzaSyBCA2oaGS7PBkEV4uelWFKaQV-KdE_3iyw',
+      libraries: ['imagery', 'places'],
+      language: 'de',
+      region: 'CH'
     })
   ],
   providers: [],
