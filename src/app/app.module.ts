d1a9db5d0facf536f7dac844269d1d9785d59bee
Sam <nagbe.samuel@gmail.com>
2021-10-21 18:57:15 +0200

    [VEL-27] - added delivery.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,39 @@
-import {LOCALE_ID, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {DeliveriesComponent} from './deliveries/deliveries.component';
-import {environment} from "../environments/environment";
-import {AngularFireModule} from "@angular/fire/compat";
-import {AddOrderComponent} from './add-order/add-order.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {NavbarComponent} from './navbar/navbar.component';
-import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
-import {AngularFireStorageModule} from '@angular/fire/compat/storage';
-import {AngularFireAuthModule} from '@angular/fire/compat/auth';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {GlobalComponents} from "./global-components";
+import { AppComponent } from './app.component';
+import { DeliveriesComponent } from './deliveries/deliveries.component';
+import { environment } from "../environments/environment";
+import { AngularFireModule } from "@angular/fire/compat";
+import { AddOrderComponent } from './add-order/add-order.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NavbarComponent } from './navbar/navbar.component';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GlobalComponents } from "./global-components";
+import { DeliveryComponent } from "./deliveries/delivery/delivery.component";
 
 //Angular Material
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatAutocompleteModule} from "@angular/material/autocomplete";
-import {DateAdapter, MatNativeDateModule, MatOptionModule} from "@angular/material/core";
-import {MatInputModule} from "@angular/material/input";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {CustomersComponent} from './customers/customers.component';
-import {MatIconModule} from '@angular/material/icon';
-import {RoadComponent} from './road/road.component';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { DateAdapter, MatNativeDateModule, MatOptionModule } from "@angular/material/core";
+import { MatInputModule } from "@angular/material/input";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { CustomersComponent } from './customers/customers.component';
+import { MatIconModule } from '@angular/material/icon';
+import { RoadComponent } from './road/road.component';
 import { CustomDateAdapter } from './custom-date-adapters';
 import { registerLocaleData } from "@angular/common";
 import localeFr from "@angular/common/locales/de";
+
 registerLocaleData(localeFr);
 
 //Google Maps
-import {AgmCoreModule} from '@agm/core';
-import {AutocompleteComponent} from "./autocomplete/autocomplete.component";
+import { AgmCoreModule } from '@agm/core';
+import { AutocompleteComponent } from "./autocomplete/autocomplete.component";
 
 @NgModule({
   declarations: [
@@ -41,7 +43,8 @@ import {AutocompleteComponent} from "./autocomplete/autocomplete.component";
     NavbarComponent,
     CustomersComponent,
     RoadComponent,
-    AutocompleteComponent
+    AutocompleteComponent,
+    DeliveryComponent
   ],
   imports: [
     BrowserModule,
@@ -73,10 +76,11 @@ import {AutocompleteComponent} from "./autocomplete/autocomplete.component";
       language: 'de'
     })
   ],
+
   providers: [
     GlobalComponents,
-    {provide: DateAdapter, useClass: CustomDateAdapter },
-    {provide: LOCALE_ID, useValue: 'de'}
+    { provide: DateAdapter, useClass: CustomDateAdapter },
+    { provide: LOCALE_ID, useValue: 'de' }
   ],
   bootstrap: [AppComponent]
 })
