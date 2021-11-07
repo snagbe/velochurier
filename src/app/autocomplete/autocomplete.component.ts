7346aa160d7774d9139b18a81ff897e45d0e036e
felixkopp <felix.kopp>
2021-11-07 17:46:25 +0100

    [VEL-69] fix problems with autocomplete component

--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -31,10 +31,9 @@ export class AutocompleteComponent implements OnInit {
     );
   }
 
-  private _filter(value: string): OrderControl[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  private _filter(value: OrderControl): OrderControl[] {
+    const filterValue = value ? value.name? value.name.toLowerCase(): value : "";
+    return this.options.filter(option => option.name.toLowerCase().includes(<string>filterValue));
   }
 
   getOptionText(option) {
